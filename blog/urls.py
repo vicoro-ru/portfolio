d7906d0e4da72c7ba833dc7ffd575f0474cbd836
Mike <ivanov_mv@viro.edu.ru>
2022-04-28 00:29:38 +0300

    after 42 lesson

--- blog/urls.py
+++ blog/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from portfolio.views import Home 
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home)
+    path('', views.Home)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
 def Home(request):
-    return render(request, 'portfolio/home.html')
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
