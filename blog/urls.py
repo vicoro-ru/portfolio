273d659c3558f6613a4c4e8ee508a6e8decb89ca
Mike <ivanov_mv@viro.edu.ru>
2022-04-27 23:15:15 +0300

    complete homework

--- blog/urls.py
+++ blog/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio.views import Home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
