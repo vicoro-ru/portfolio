33eff808f252d23053aa65d4df9cb00af78ca98b
Mike <ivanov_mv@viro.edu.ru>
2022-04-29 01:55:12 +0300

    Create Blog

--- miniBlog/models.py
+++ miniBlog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class MiniBlog(models.Model):
+    postName = models.CharField(max_length=100)
+    publisTime = models.DateField(auto_now=True)
+    description = models.TextField()
+    postText = models.TextField()
--- miniBlog/views.py
+++ miniBlog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import MiniBlog
 
 # Create your views here.
+
+def Blog(request):
+    blog = MiniBlog.objects.order_by('-publisTime')[:5]
+    return render(request, 'miniBlog/blog.html', {'blog': blog})
